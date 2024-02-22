d3aa7a8e2c587f55d7024a65f9accff74154e170
blacktemplarsfanaccount <146381500+blacktemplarsfanaccount@users.noreply.github.com>
2024-02-22 14:44:20 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Hello_world
+{
+    //Main class 
+    internal class Program
+    {   // The Main method is executed when  application starts
+        static void Main(string[] args)
+        {   // Asks the user for first name
+            Console.WriteLine("What is your first name");
+            // Assigns user input to FirstName variable
+            string firstName = Console.ReadLine();
+            // Asks the user for last name
+            Console.WriteLine("What is your last name");
+            // Assigns user input to LastName variable
+            string lastName = Console.ReadLine();
+            
+            // Prints a welcome message  with firstName and lastName
+            Console.WriteLine("Hello," + firstName +  " " +  lastName + "!"); 
+
+
+
+
+        }
+
+       
+    }
+}
